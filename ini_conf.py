fcd03c2e9f73c31209512c82a9e3e9d412beb093
SmellyCat <smellycat2014@foxmail.com>
2016-03-16 18:37:12 +0800

    更新 配置文件获取

--- ini_conf.py
+++ ini_conf.py
@@ -1,43 +1,34 @@
-
+#-*- encoding: utf-8 -*-
 import ConfigParser
 
 class MyIni:
-    def __init__(self, conf_path='my_ini.conf'):
+
+    def __init__(self, conf_path = 'my_ini.conf'):
         self.conf_path = conf_path
         self.cf = ConfigParser.ConfigParser()
         self.cf.read(conf_path)
 
     def get_kakou(self):
+        """获取卡口配置文件"""
         conf = {}
         section = 'KAKOU'
-        conf['host'] = self.cf.get(section, 'host')
-        conf['port'] = self.cf.getint(section, 'port')
+        conf['host']    = self.cf.get(section, 'host')
+        conf['port']    = self.cf.getint(section, 'port')
         conf['id_flag'] = self.cf.getint(section, 'id_flag')
-        conf['id_step'] = self.cf.getint(section, 'id_step')
-        conf['time_flag'] = self.cf.get(section, 'time_flag')
-        conf['time_step'] = self.cf.getint(section, 'time_step')
-        conf['kkdd'] = self.cf.get(section, 'kkdd')
-        conf['city'] = self.cf.get(section, 'city')
+        conf['city']    = self.cf.get(section, 'city')
         return conf
 
-    def get_hbc(self):
+    def get_union(self):
+        """获取联网卡口配置文件信息"""
         conf = {}
-        section = 'HBC'
+        section = 'UNION'
         conf['host'] = self.cf.get(section, 'host')
         conf['port'] = self.cf.getint(section, 'port')
         return conf
 
     def set_id(self, id_flag):
+        """设置id标记"""
         self.cf.set('KAKOU', 'id_flag', id_flag)
         self.cf.write(open(self.conf_path, 'w'))
 
-    def set_time(self, time_flag):
-        self.cf.set('KAKOU', 'time_flag', time_flag)
-        self.cf.write(open(self.conf_path, 'w'))
-
-if __name__ == '__main__':
-    ini = MyIni()
-    print ini.get_kakou()
-    print ini.get_hbc()
-    #ini.set_time('2015-10-01 00:10:23')
-    #print hbc
+    
