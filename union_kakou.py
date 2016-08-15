63ee5767cd3f7642dad25250858e31bcdb4ee05d
SmellyCat <smellycat2014@foxmail.com>
2016-08-15 22:06:57 +0800

    添加 连接测试方法

--- union_kakou.py
+++ union_kakou.py
@@ -30,3 +30,16 @@ class UnionKakou(object):
             self.status = False
             raise
 
+    def connect_test(self):
+        url = 'http://{0}:{1}'.format(self.host, self.port)
+        try:
+            r = requests.get(url)
+            if r.status_code == 200:
+                return json.loads(r.text)
+            else:
+                self.status = False
+                raise Exception('url: {url}, status: {code}, {text}'.format(
+                    url=url, code=r.status_code, text=r.text))
+        except Exception as e:
+            self.status = False
+            raise
