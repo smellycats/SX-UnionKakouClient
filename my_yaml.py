a74dbcfba88a99da5f21f7ecdc28a0e3a9c915e0
SmellyCat <smellycat2014@foxmail.com>
2017-05-09 15:22:19 +0800

    添加 yaml

---
+++ my_yaml.py
@@ -0,0 +1,22 @@
+#-*- encoding: utf-8 -*-
+import ruamel.yaml
+
+
+class MyYAML(object):
+    def __init__(self, path = 'my.yaml'):
+        self.path = path
+
+    def __del__(self):
+        pass
+
+    def get_ini(self):
+        f = open(self.path, 'r')
+        return ruamel.yaml.load(
+            stream=f, Loader=ruamel.yaml.RoundTripLoader)
+        f.close()
+
+    def set_ini(self, data):
+        f = open(self.path, 'w')
+        ruamel.yaml.dump(data, stream=f, Dumper=ruamel.yaml.RoundTripDumper,
+                         default_flow_style=False, allow_unicode=True)
+        f.close()
