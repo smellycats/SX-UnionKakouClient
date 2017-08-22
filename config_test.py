e13131769fcdd8b3becd9aa422cc8320626fef2a
SmellyCat <smellycat2014@foxmail.com>
2017-08-22 21:07:06 +0800

    更新 使用yaml

--- config_test.py
+++ config_test.py
@@ -1,19 +1,27 @@
-from ini_conf import MyIni
+#-*- encoding: utf-8 -*-
+from my_yaml import MyYAML
 
-def test_get_ini():
-    mi = MyIni()
-    assert 'host' in mi.get_kakou()
-    assert 'port' in mi.get_kakou()
-    assert 'id_flag' in mi.get_kakou()
-    assert 'city' in mi.get_kakou()
-    assert 'host' in mi.get_union()
-    assert 'port' in mi.get_union()
 
-def test_set_id_flag():
-    mi = MyIni()
-    mi.set_id(709394)
+class TestYAML(object):
+    def __init__(self):
+        self.my_ini = MyYAML()
+        
+    def get_ini(self):
+        print dict(self.my_ini.get_ini()['kakou'])
+        print dict(self.my_ini.get_ini()['union'])
+        print list(self.my_ini.get_ini()['usefulkkdd'])
+        print type(self.my_ini.get_ini()['city'])
+        print type(self.my_ini.get_ini()['kkdd_id'])
+        print type(self.my_ini.get_ini()['id_flag'])
+        print self.my_ini.get_ini()['id_step']
 
+    def set_ini(self):
+        data = self.my_ini.get_ini()
+        data['usefulkkdd'] = ['789', '456', '123', 'user']
+        print self.my_ini.set_ini(data)
 
 if __name__ == "__main__":
-    test_get_ini()
-    test_set_id_flag()
+    ty = TestYAML()
+    #ty.get_ini()
+    ty.set_ini()
+    #ty.set_ini()
