611b4f82ef7100fc386dc3459b861c21d0562089
SmellyCat <smellycat2014@foxmail.com>
2017-08-22 21:06:33 +0800

    更改 使用with as 方法

--- my_yaml.py
+++ my_yaml.py
@@ -3,19 +3,19 @@ import ruamel.yaml
 
 
 class MyYAML(object):
-    def __init__(self, path = 'my.yaml'):
+    def __init__(self, path='my.yaml'):
         self.path = path
 
     def __del__(self):
         pass
 
     def get_ini(self):
-        f = open(self.path, 'r')
-        return ruamel.yaml.load(stream=f, Loader=ruamel.yaml.RoundTripLoader)
-        f.close()
+        with open(self.path, 'r') as f:
+            return ruamel.yaml.load(
+                stream=f, Loader=ruamel.yaml.RoundTripLoader)
 
     def set_ini(self, data):
-        f = open(self.path, 'w')
-        ruamel.yaml.dump(data, stream=f, Dumper=ruamel.yaml.RoundTripDumper,
-                         default_flow_style=False, allow_unicode=True)
-        f.close()
+        with open(self.path, 'w') as f:
+            ruamel.yaml.dump(
+                data, stream=f, Dumper=ruamel.yaml.RoundTripDumper,
+                default_flow_style=False, allow_unicode=True)
