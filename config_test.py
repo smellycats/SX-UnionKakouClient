1254211681a3a249507acd03d35cbf8b57290eb7
SmellyCat <smellycat2014@foxmail.com>
2017-11-28 17:25:28 +0800

    更改 测试代码

--- config_test.py
+++ config_test.py
@@ -7,14 +7,8 @@ class TestYAML(object):
         self.my_ini = MyYAML()
         
     def get_ini(self):
-        print dict(self.my_ini.get_ini()['kakou'])
-        print dict(self.my_ini.get_ini()['union'])
-        print list(self.my_ini.get_ini()['usefulkkdd'])
-        print type(self.my_ini.get_ini()['city'])
-        print type(self.my_ini.get_ini()['kkdd_id'])
-        print type(self.my_ini.get_ini()['id_flag'])
-        print self.my_ini.get_ini()['id_step']
-
+        print self.my_ini.get_ini()['usefulkkdd']
+        
     def set_ini(self):
         data = self.my_ini.get_ini()
         data['usefulkkdd'] = ['789', '456', '123', 'user']
@@ -22,6 +16,6 @@ class TestYAML(object):
 
 if __name__ == "__main__":
     ty = TestYAML()
-    #ty.get_ini()
-    ty.set_ini()
+    ty.get_ini()
+    #ty.set_ini()
     #ty.set_ini()
